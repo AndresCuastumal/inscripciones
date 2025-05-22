b4b8b3cbda504c9613ab472df5bb4f8cb7fcc6d2
andrescuastumal <andrescuastumal@gmail.com>
2025-05-22 17:22:04 -0500

    correccion_conexion2

--- config/conexion.php
+++ config/conexion.php
@@ -1,24 +1,31 @@
 <?php
 date_default_timezone_set('America/Bogota');
-require_once __DIR__ . '/vendor/autoload.php'; //carga composer al proyecto.
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); //crea instancia de la clase dotenv
-$dotenv->load(); //va a la carpeta raiz, busca el archivo .env de variables de entorno 
-  $dotenv->required(['host', 'db', 'usuario', 'password']);
-  $host = $_ENV['host'];
-  $name = $_ENV['db'];
-  $name2 = $_ENV['db2'];
-  $user = $_ENV['usuario'];
-  $pass = $_ENV['password'];
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['host', 'db', 'usuario', 'password']);
+
+$host = $_ENV['host'];
+$name = $_ENV['db'];
+$name2 = $_ENV['db2'];
+$user = $_ENV['usuario'];
+$pass = $_ENV['password'];
+
 try {
-        $conn=new PDO("mysql:host=$host;dbname=$name; charset=utf8", $user,$pass);        
-    } catch (Exception $errorconexion) {
-        echo $errorconexion->getMessage();
-    }
-    //para actualizasr y enviar a servidor
-    try {
-      $conn2=new PDO("mysql:host=$host;dbname=$name2; charset=utf8", $user,$pass);        
-  } catch (Exception $errorconexion) {
-      echo $errorconexion->getMessage();
-  }
-  //para actualizasr y enviar a servidor
+    $conn = new PDO("mysql:host=$host;dbname=$name;charset=utf8", $user, $pass);
+} catch (Exception $e) {
+    header('Content-Type: application/json');
+    echo json_encode(["error" => "Error de conexión a la base de datos principal: " . $e->getMessage()]);
+    exit;
+}
+
+try {
+    $conn2 = new PDO("mysql:host=$host;dbname=$name2;charset=utf8", $user, $pass);
+} catch (Exception $e) {
+    header('Content-Type: application/json');
+    echo json_encode(["error" => "Error de conexión a la segunda base de datos: " . $e->getMessage()]);
+    exit;
+}
 ?>
